43a92fad4f5cd06d03b4a3b93a0b476e57cfa7d9
Phil Lindsay <philiplindsay25@gmail.com>
2020-09-09 06:39:56 -0400

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using FutureValues2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FutureValues2.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Phil";
+            ViewBag.FV = 99999.99;
+            return View();
+        }
+        [HttpPost] 
+        public IActionResult Index(FutureValueModel model) 
+        { 
+            if (ModelState.IsValid) 
+            { 
+                ViewBag.FV = model.CalculateFutureValue(); 
+            } 
+            else 
+            {
+                ViewBag.FV = 0;
+            } 
+            return View(model); 
+        }
+    }
+}
